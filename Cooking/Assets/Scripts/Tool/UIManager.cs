20a15659daeda0992cd3b5db36531ad64b4bf4ae
秦唐雨洋 <94535848+qintangyuyang@users.noreply.github.com>
2026-06-13 01:01:32 +0800

    代码

--- Cooking/Assets/Scripts/Tool/UIManager.cs
+++ Cooking/Assets/Scripts/Tool/UIManager.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager
+/// <summary>
+/// UI管理器（Monobehaviour单例）
+/// </summary>
+public class UIManager : MonoBehaviour
 {
     private static UIManager _instance;
 
@@ -12,26 +16,48 @@ public class UIManager
         {
             if (_instance == null)
             {
-                _instance = new UIManager();
+                _instance = FindObjectOfType<UIManager>();
+                if (_instance == null)
+                {
+                    var go = new GameObject("UIManager");
+                    _instance = go.AddComponent<UIManager>();
+                }
             }
             return _instance;
         }
     }
+    
+    //是否初始化的bool值
+    private bool isInitialized = false;
 
     //一开始就获取游戏场景中的Canvas，以便后续操作
     private Transform CanvasTransform;
 
-    private UIManager()
+    private void Awake()
     {
-        CanvasTransform = GameObject.Find("Canvas").transform;
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    private void Init()
+    {
+        if (isInitialized)
+            return;
+        CanvasTransform = this.transform.Find("Canvas").transform;
         if (CanvasTransform != null)
         {
-            GameObject.DontDestroyOnLoad(CanvasTransform.transform.parent.gameObject);
+            //将场景中Canvas的父对象设置为过场景不移除
+            //也就是该脚本挂载的节点 UIRoot
+            GameObject.DontDestroyOnLoad(this.gameObject);
         }
         else
         {
             Debug.LogError("Canvas not found");
         }
+        isInitialized = true;
     }
     
     //打开面板

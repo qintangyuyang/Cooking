ee01e8c6689a9c4bb522090a6c8835b11b13ea6c
秦唐雨洋 <94535848+qintangyuyang@users.noreply.github.com>
2026-06-13 00:07:02 +0800

    部分UIManager代码

--- Cooking/Assets/Scripts/Tool/UIManager.cs
+++ Cooking/Assets/Scripts/Tool/UIManager.cs
@@ -2,16 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager
 {
-    void Start()
+    private static UIManager _instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new UIManager();
+            }
+            return _instance;
+        }
+    }
+
+    //一开始就获取游戏场景中的Canvas，以便后续操作
+    private Transform CanvasTransform;
+
+    private UIManager()
+    {
+        CanvasTransform = GameObject.Find("Canvas").transform;
+        if (CanvasTransform != null)
+        {
+            GameObject.DontDestroyOnLoad(CanvasTransform.transform.parent.gameObject);
+        }
+        else
+        {
+            Debug.LogError("Canvas not found");
+        }
+    }
+    
+    //打开面板
+    public T OpenUI<T>() where T : UIBase
+    {
+        return default(T);
+    }
+    
+    //关闭面板
+    public void CloseUI<T>() where T : UIBase
     {
         
     }
-
     
-    void Update()
+    //获得面板
+    public T GetUI<T>() where T : UIBase
     {
-        
+        return default(T);
     }
 }
